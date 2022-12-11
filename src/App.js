89c84a11ef4b19dc55537119f085ceb616b4d143
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2022-12-11 09:36:24 +0800

    basic HTML and CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import Weather from "./Weather.js";
+
 function App() {
   return (
     <div className="App">
--- src/Weather.js
+++ src/Weather.js
@@ -1,52 +1,67 @@
 import React from "react";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div>
-      <h1>
-        It is clear in <span>Perth</span>
-      </h1>
-      <div className="row">
+    <div className="Weather">
+      <div className=".Box Centered">
+        <form className="SearchForm">
+          <h1 className="Centered">
+            It is clear in <span> </span>
+          </h1>
+          <input className="InputForm" type="search" value="Perth" />
+        </form>
+      </div>
+      <div className="row Centered">
         <div className="col-4">🌧</div>
         <div className="col-4">25°C</div>
         <div className="col-4">
-          <ul>
+          <ul className="ListStyle">
             <li>🌫 8 km/h</li>
             <li>☂ 0%</li>
             <li>💧 30%</li>
           </ul>
         </div>
       </div>
-      <div className="row">
-        <div className="col">
-          <li>Mon</li>
-          <li>🌧</li>
-          <li>13°</li>
+      <div className="row Centered">
+        <div className="col-3">
+          <ul className="ListStyle">
+            <li>Mon</li>
+            <li>🌧</li>
+            <li>13°</li>
+          </ul>
         </div>
-        <div className="col">
-          <li>Tues</li>
-          <li>🌧</li>
-          <li>13°</li>
+        <div className="col-3">
+          <ul className="ListStyle">
+            <li>Tues</li>
+            <li>🌧</li>
+            <li>13°</li>
+          </ul>
         </div>
-        <div className="col">
-          <li>Wed</li>
-          <li>🌧</li>
-          <li>13°</li>
+        <div className="col-3">
+          <ul className="ListStyle">
+            <li>Wed</li>
+            <li>🌧</li>
+            <li>13°</li>
+          </ul>
         </div>
-        <div className="col">
-          <li>Thurs</li>
-          <li>🌧</li>
-          <li>13°</li>
+        <div className="col-3">
+          <ul className="ListStyle">
+            <li>Thurs</li>
+            <li>🌧</li>
+            <li>13°</li>
+          </ul>
         </div>
-        <div className="col">
-          <li>Fri</li>
-          <li>🌧</li>
-          <li>13°</li>
+      </div>
+      <div className="Box">
+        <div>
+          {" "}
+          <a href="/">C°</a>
         </div>
-        <div className="col">
-          <li>Sat</li>
-          <li>🌧</li>
-          <li>13°</li>
+        <div> | </div>
+        <div>
+          {" "}
+          <a href="/">F°</a>
         </div>
       </div>
     </div>
