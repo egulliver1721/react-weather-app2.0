24ae141ad33ca5bea13789f4b41a27e4ae8c10f9
egulliver1721 <118499423+egulliver1721@users.noreply.github.com>
2022-12-12 08:59:33 +0800

    removed C to F conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,91 +1,29 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Weather.css";
-import WeatherIcon from "./WeatherIcon.js";
 
 import FormattedDate from "./FormattedDate.js";
 
 export default function WeatherInfo(props) {
-  const [unit, setUnit] = useState("celcius");
-
-  function convertToFarenheit(event) {
-    event.preventDefault();
-    setUnit("farenheit");
-  }
-
-  function convertToCelcius(event) {
-    event.preventDefault();
-    setUnit("celcius");
-  }
-
-  function farenheit() {
-    return (props.data.temperature * 9) / 5 + 32;
-  }
-
-  if (unit === "celcius") {
-    return (
-      <div className="CurrentWeather">
-        <div className="row">
-          <div className="col-4 Icon Centered">
-            <img src={props.data.icon} alt="" className="Icon" />
-          </div>
-          <div className="col-4 CurrentTemperature Centered">
-            {Math.round(props.data.temperature)}°
-          </div>
-          <div className="col-4 WeatherInformation">
-            <ul className="ListStyle">
-              <li>
-                <FormattedDate date={props.data.date} />
-              </li>
-              <li>🌫 {Math.round(props.data.wind)} km/h</li>
-              <li>💧 {props.data.humidity}%</li>
-              <li className="Capitalise">{props.data.description}</li>
-            </ul>
-          </div>
+  return (
+    <div className="CurrentWeather">
+      <div className="row">
+        <div className="col-4 Icon Centered">
+          <img src={props.data.icon} alt="" className="Icon" />
         </div>
-        <div className="Box CelciusConversion">
-          <div className="Metric">C°</div>
-          <span> | </span>
-          <div className="Metric">
-            {" "}
-            <a href="/" onClick={convertToFarenheit}>
-              F°
-            </a>
-          </div>
+        <div className="col-4 CurrentTemperature Centered">
+          {Math.round(props.data.temperature)}°
+        </div>
+        <div className="col-4 WeatherInformation">
+          <ul className="ListStyle">
+            <li>
+              <FormattedDate date={props.data.date} />
+            </li>
+            <li>🌫 {Math.round(props.data.wind)} km/h</li>
+            <li>💧 {props.data.humidity}%</li>
+            <li className="Capitalise">{props.data.description}</li>
+          </ul>
         </div>
       </div>
-    );
-  } else {
-    return (
-      <div className="CurrentWeather">
-        <div className="row">
-          <div className="col-4 Icon Centered">
-            <img src={props.data.icon} alt="" className="Icon" />
-          </div>
-          <div className="col-4 CurrentTemperature Centered">
-            {Math.round(farenheit())}°
-          </div>
-          <div className="col-4 WeatherInformation">
-            <ul className="ListStyle">
-              <li>
-                <FormattedDate date={props.data.date} />
-              </li>
-              <li>🌫 {Math.round(props.data.wind)} km/h</li>
-              <li>💧 {props.data.humidity}%</li>
-              <li className="Capitalise">{props.data.description}</li>
-            </ul>
-          </div>
-        </div>
-        <div className="Box CelciusConversion">
-          <div className="Metric">
-            {" "}
-            <a href="/" onClick={convertToCelcius}>
-              C°
-            </a>
-          </div>
-          <span> | </span>
-          <div className="Metric">F°</div>
-        </div>
-      </div>
-    );
-  }
+    </div>
+  );
 }
